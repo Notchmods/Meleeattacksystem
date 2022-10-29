3c48db25b5107df54e039bb4c936456d59efdf90
Notchmods <Nomics3000@gmail.com>
2022-10-29 23:16:26 +1100

    Melee script

---
+++ Meleeprototype/Assets/Script/Animationscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animationscript : MonoBehaviour
+{
+    public Animator Meleeanimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Animationcorroutine());
+    }
+
+    IEnumerator Animationcorroutine()
+    {
+        Meleeanimation.SetBool("Attack", true);
+        yield return new WaitForSeconds(1f);
+        Meleeanimation.SetBool("Attack", false);
+    }
+}
